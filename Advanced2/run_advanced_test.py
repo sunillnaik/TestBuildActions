3f41aee3817e4a39684d052828fd8991e3236c1d
Sunil Naik <sunillnaik@gmail.com>
2020-01-09 18:32:24 +0900

    Fix advanced2 errors

--- Advanced2/run_advanced_test.py
+++ Advanced2/run_advanced_test.py
@@ -1,4 +1,7 @@
-print("Advanced Test execution starts")
-for x in range(10):
-  print("ADVANCED TEST PASS - " + str(x))
-  return 0
+def main():
+    print("Advanced Test execution starts")
+    for x in range(10):
+      print("ADVANCED TEST PASS - " + str(x))
+      return 0
+
+main()
